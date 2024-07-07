787ed355beb5158547709a1c8a754fd30b7a22ac
alchemyyt <josemontesdeoca00@gmail.com>
2024-07-06 22:42:55 -0400

    edit cosplay.jsx

--- src/app/components/Cosplay.jsx
+++ src/app/components/Cosplay.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Link from "next/link";
 import { lastElements } from "../services/lastElements";
+
 const Cosplay = ({ data }) => {
   const lastPosts = lastElements(4, data);
   return (
     <div>
-      <h1>cosplay</h1>
+      <Link href={"#"}>
+        <h2 className="hover:text-amber-500 text-xl ">Noticiass</h2>
+      </Link>
       <ul>
         {lastPosts.map((element) => (
           <li className="border border-black bg-ambar-500" key={element.id}>
